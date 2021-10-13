198942e6ede6023eb7f96f3efc26433b22a64ee3
Anas EL MASSAFI <45128409+Elmassafi@users.noreply.github.com>
2021-10-14 00:09:06 +0100

    show only activated clients (#1)

--- src/app/pages/espaceclient/service/Client.ts
+++ src/app/pages/espaceclient/service/Client.ts
@@ -1,51 +1,47 @@
-import {Injectable} from "@angular/core";
-import {DatePipe} from "@angular/common";
+import {Injectable} from '@angular/core';
+import {DatePipe} from '@angular/common';
 
 @Injectable()
-export class Client
-{
-  id:number;
-  nom:string;
-  prenom:string;
-  cine:string;
-  rc:string;
-  nom_societe:string;
-  tel:string;
-  email:string;
-  adresse:string;
-  id_maloc:number;
-  verifie:boolean;
-  actif:boolean;
-  type:number;
-  comptes:Array<Compte>;
-  constructor()
-  {
-    this.type=1;
-    this.comptes=[];
+export class Client {
+  id: number;
+  nom: string;
+  prenom: string;
+  cine: string;
+  rc: string;
+  nom_societe: string;
+  tel: string;
+  email: string;
+  adresse: string;
+  id_maloc: number;
+  verifie: number;
+  actif: number;
+  type: number;
+  comptes: Array<Compte>;
+
+  constructor() {
+    this.type = 1;
+    this.comptes = [];
   }
 }
-export class Compte
-{
-  id:number;
-  login:string;
-  email:string;
-  password:string;
-  tel:string;
-  
-  constructor(c?:Client)
-  {
-    if(c){
-      console.log(c);
-    var datePipe = new DatePipe('en-US');
-    this.password=datePipe.transform(new Date(),"ddMMyyyy");
-    this.email=c.email;
-    this.login=c.nom?c.nom:c.nom_societe;
-    this.tel = c.tel;
-  }
-    
-  }
 
-  
+export class Compte {
+  id: number;
+  login: string;
+  email: string;
+  password: string;
+  tel: string;
+
+  constructor(c?: Client) {
+    if (c) {
+      console.log(c);
+      const datePipe = new DatePipe('en-US');
+      this.password = datePipe.transform(new Date(), 'ddMMyyyy');
+      this.email = c.email;
+      this.login = c.nom ? c.nom : c.nom_societe;
+      this.tel = c.tel;
+    }
+
+  }
 
 
 }
--- src/app/pages/espaceclient/service/client.service.ts
+++ src/app/pages/espaceclient/service/client.service.ts
@@ -1,124 +1,113 @@
 ///<reference path="../interface/client.interface.ts"/>
-import { Injectable } from "@angular/core";
-import { ClientInterface } from "../interface/client.interface";
-import { HttpClient } from "@angular/common/http";
-import { Promise } from "q";
-import { promise } from "selenium-webdriver";
-import { Observable } from "rxjs";
-import { Client, Compte } from "./Client";
-import { config } from '../../../config';
-import { LoginService } from '../../../login.service';
+import {Injectable} from '@angular/core';
+import {ClientInterface} from '../interface/client.interface';
+import {HttpClient} from '@angular/common/http';
+import {Promise} from 'q';
+import {Observable} from 'rxjs';
+import {Client, Compte} from './Client';
+import {config} from '../../../config';
+import {LoginService} from '../../../login.service';
 
 @Injectable()
 export class ClientService extends ClientInterface {
   id; number;
   client: Client;
+
   client_exist(client: Client): Promise<any> {
-    let promise = Promise((resolve, reject) => {
-      this.http.get<any>(`${config.apiUrl}/bo/` + this.loginService.getJwtToken() + "/client_exist?s=" + client.nom_societe).toPromise()
+    return Promise((resolve, reject) => {
+      this.http
+        .get<any>(`${config.apiUrl}/bo/` + this.loginService.getJwtToken() + '/client_exist?s=' + client.nom_societe)
+        .toPromise()
         .then(data => {
-          console.log("success");
-          resolve(data);
-        }, msg => {
-          console.log("errors");
-          reject(msg);
-        }
-        )
+            console.log('success');
+            resolve(data);
+          }, msg => {
+            console.log('errors');
+            reject(msg);
+          },
+        );
 
     });
-    return promise;
   }
 
   compte_exist(compte: Compte): Promise<any> {
-    let promise = Promise((resolve, reject) => {
-      this.http.get<any>(`${config.apiUrl}/bo/` + this.loginService.getJwtToken() + "/compte_exist?login=" + compte.login).toPromise()
+    return  Promise((resolve, reject) => {
+      this.http
+        .get<any>(`${config.apiUrl}/bo/` + this.loginService.getJwtToken() + '/compte_exist?login=' + compte.login)
+        .toPromise()
         .then(data => {
-          console.log("success");
-          resolve(data);
-        }, msg => {
-          console.log("errors");
-          reject(msg);
-        }
-        )
+            console.log('success');
+            resolve(data);
+          }, msg => {
+            console.log('errors');
+            reject(msg);
+          },
+        );
     });
-    return promise;
   }
+
   constructor(private loginService: LoginService, public http: HttpClient) {
     super();
 
   }
+
   add_client(client: Client): Promise<any> {
     console.log(client);
-    let promise = Promise((resolve, reject) => {
-      this.http.post<any>(`${config.apiUrl}/bo/` + this.loginService.getJwtToken() + "/add_client", client).toPromise()
+    return  Promise((resolve, reject) => {
+      this.http.post<any>(`${config.apiUrl}/bo/` + this.loginService.getJwtToken() + '/add_client', client).toPromise()
         .then(data => {
-          console.log(data);
-          console.log("success");
-        }, msg => {
+            console.log(data);
+            console.log('success');
+          }, msg => {
 
-          console.log("errors");
-          reject(msg);
-        }
-        )
+            console.log('errors');
+            reject(msg);
+          },
+        );
 
     });
-    return promise;
   }
 
-getCompte():Observable<Compte[]>{
-  return this.http.get<Compte[]>(`${config.apiUrl}/compte`);
-}
-getClient():Observable<Client[]>{
-  return this.http.get<Client[]>(`${config.apiUrl}/client`);
-}
-/*
-delete_client(id: number): Observable<any> {
-  return this.http.delete<Client[]>(`${config.apiUrl}/delete`);
-}*/
-update_client(client: Client): Promise<any> {
-  console.log(client);
-  let promise = Promise((resolve, reject) => {
-    this.http.put<any>(`${config.apiUrl}/bo/` + this.loginService.getJwtToken() + "/update_client",client).toPromise()
-      .then(data => {
-        console.log("success");
-        resolve(data);
+  getCompte(): Observable<Compte[]> {
+    return this.http.get<Compte[]>(`${config.apiUrl}/compte`);
+  }
 
-      }, msg => {
+  getClient(): Observable<Client[]> {
+    return this.http.get<Client[]>(`${config.apiUrl}/client`);
+  }
 
-        console.log("errors");
-        reject(msg);
-      }
-      )
+  /*
+  delete_client(id: number): Observable<any> {
+    return this.http.delete<Client[]>(`${config.apiUrl}/delete`);
+  }*/
+  update_client(client: Client): Promise<any> {
+    console.log(client);
+    return  Promise((resolve, reject) => {
+      this.http
+        .put<any>(`${config.apiUrl}/bo/` + this.loginService.getJwtToken() + '/update_client', client)
+        .toPromise()
+        .then(data => {
+            console.log('success');
+            resolve(data);
 
-  }); return promise;
-}
+          }, msg => {
 
-desactiver(client: Client): Promise<any> {
-  console.log(client);
-  let promise = Promise((resolve, reject) => {
-    this.http.put<any>(`${config.apiUrl}/bo/` + this.loginService.getJwtToken() + "/desactiver",client).toPromise()
-      .then(data => {
-        console.log("success");
-        resolve(data);
+            console.log('errors');
+            reject(msg);
+          },
+        );
 
-      }, msg => {
+    });
+  }
 
-        console.log("errors");
-        reject(msg);
-      }
-      )
-
-  }); return promise;
-}
+  desactiver(client: Client): Observable<any> {
+    console.log(JSON.stringify(client));
+    return this.http.put<any>(`${config.apiUrl}/bo/` + this.loginService.getJwtToken() + '/desactiver', client);
+  }
 
 
-
-
-
-
-setter(client:Client){
- this.client=client;
-}
+  setter(client: Client) {
+    this.client = client;
+  }
 
 }
- 
